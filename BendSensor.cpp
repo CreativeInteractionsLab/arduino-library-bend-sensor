2179ae91c14ee4bf146001a6cec8e3e1486d8d83
alexeady <alexander.k.eady@gmail.com>
2017-08-14 14:31:26 -0400

    Fixed some errors in the bend sensor librare
    *currently does not support multiple bend sensors - this needs to be fixed ASAP*

--- BendSensor.cpp
+++ BendSensor.cpp
@@ -6,6 +6,7 @@
 #include "BendSensor.h"
 
 #define CALIBRATE_ARRAY_MAX 10
+#define SENSOR_MODE 512
 
 BendSensor::BendSensor(){
 
@@ -16,81 +17,130 @@ BendSensor::~BendSensor(){
 }
 
 void BendSensor::initialize(int sensorPin){
-  _calibrate[CALIBRATE_ARRAY_MAX];
-  _pin = sensorPin;
-  _threshold = 0;
-  _restingVal = 512;
-  _calibrateCounter = 0;
+  this->_calibrate[CALIBRATE_ARRAY_MAX];
+  this->_pin = sensorPin;
+  this->_threshold = 0;
+  this->_restingVal = SENSOR_MODE;
+  this->_calibrateCounter = 0;
+  this->_calibrated = false;
 
   for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
-    _calibrate[i] = 0;
+    this->_calibrate[i] = SENSOR_MODE;
   }  
 }
 
 void BendSensor::initialize(int sensorPin, int threshold){
-  _calibrate[CALIBRATE_ARRAY_MAX];
-  _pin = sensorPin;
-  _threshold = threshold;
-  _restingVal = 512;
-  _calibrateCounter = 0;
+  this->_calibrate[CALIBRATE_ARRAY_MAX];
+  this->_pin = sensorPin;
+  this->_threshold = threshold;
+  this->_restingVal = SENSOR_MODE;
+  this->_calibrateCounter = 0;
+  this->_calibrated = false;
 
   for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
-    _calibrate[i] = 0;
+    this->_calibrate[i] = SENSOR_MODE;
   }  
 }
 
 void BendSensor::calibrateMode () {
   // takes up to 10 readings from the sensors and stores them to determine restingVal of sensor in unbent state
-  _calibrate[_calibrateCounter] = analogRead(_pin);
-  _calibrateCounter++;
-  if (_calibrateCounter >= CALIBRATE_ARRAY_MAX) {_calibrateCounter = 0;}  
+	if (this->_calibrated == false){
+
+		int counter = 0;
+
+		for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
+
+			if (counter <= CALIBRATE_ARRAY_MAX) {
+
+				Serial.print ("counter: ");
+				Serial.print (counter);
+				Serial.print (", reading: ");
+				Serial.print (analogRead(_pin));
+
+				this->_calibrate[counter] = analogRead(_pin);
+
+				Serial.print ("stored: ");
+				Serial.println (this->_calibrate[counter]);
+
+		  		counter++;		  		
+			}	  
+		  	
+		  	delay (100);
+	  	}
+
+	  	// sets the resting value for the sensor using calibrateMode data
+		  int average = 0;
+		  
+		  for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
+		    average += this->_calibrate[i];
+		  }
+
+		  average = average/CALIBRATE_ARRAY_MAX;
+		  this->_restingVal = average;
+
+		  Serial.print ("average: ");
+		  Serial.print (average);
+		  Serial.print (", stored: ");
+		  Serial.println (this->_restingVal);
+
+		  //sets threshold, if not already defined
+		  int difference = 0;
+		  if (this->_threshold == 0){
+		    
+		    for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
+		      if ( abs(average - this->_calibrate[i]) > difference) { difference = abs(average - this->_calibrate[i]); }
+		    }
+
+		    this->_threshold = difference * 20;
+		  }
+
+		  Serial.print ("threshold: ");
+		  Serial.print (difference * 20);
+		  Serial.print (", stored: ");
+		  Serial.println (this->_threshold);
+
+	  	this->_calibrated = true;
+	}
+  	 
 }
 
-void BendSensor::setResting () {
+void BendSensor::reset () {
   // sets the resting value for the sensor using calibrateMode data
-  int average = 0;
+  this->_calibrated = false;
   
-  for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
-    average += _calibrate[i];
-  }
-  _restingVal = average/CALIBRATE_ARRAY_MAX;
-
-  //sets threshold, if not already defined
-  if (_threshold == 0){
-    int difference = 0;
-    for (int i = 0; i < CALIBRATE_ARRAY_MAX; i++){
-      if ( abs(average - _calibrate[i]) > difference) { difference = abs(average - _calibrate[i]); }
-    }
-
-    _threshold = difference * 2;
-  }
 }
 
 int BendSensor::getState (){
   // returns one of three values (0, resting; 1, bent up; -1, bent down)
-  _currentVal = analogRead(_pin);
   
-   if (_currentVal > _restingVal + _threshold){
-    return (1);
-   } else if (_currentVal < _restingVal - _threshold) {
-    return (-1);
-   } else {
-     return (0);
-   }
-  
+  if (this->_calibrated == true) {
+	  this->_currentVal = analogRead(_pin);
+	  
+	   if (this->_currentVal > this->_restingVal + this->_threshold){
+	    return (1);
+	   } else if (this->_currentVal < this->_restingVal - this->_threshold) {
+	    return (-1);
+	   } else {
+	     return (0);
+	   }
+	}
+	else {
+		return (0);
+	}  
 }
 
 void BendSensor::report(){
-  int repPin = _pin;
+  int repPin = this->_pin;
   Serial.print ("pin: ");
   Serial.print (repPin);
 
-  int repRest = _restingVal;
+  int repRest = this->_restingVal;
   Serial.print (" , rest: ");
   Serial.print (repRest);
 
-  int repThresh = _threshold;
+  int repThresh = this->_threshold;
   Serial.print (" , threshold: ");
   Serial.println (repThresh);
 }
+
 
--- BendSensor.h
+++ BendSensor.h
@@ -15,17 +15,19 @@ class BendSensor{
     void initialize(int sensorPin); //real constructor, needs the analog pin
     void initialize(int sensorPin, int threshold); //real constructor, needs the analog pin, and threshold (amount to consider bent) to be passed to it
     void calibrateMode (); // takes up to 10 readings from the sensors and stores them to determine restingVal of sensor in unbent state
-    void setResting ();    // sets the resting value for the sensor using calibrateMode data
+    void reset();		   // sets _calibrated = false, so that the sensors will calibrate again
     int  getState ();      // returns one of three values (0, resting; 1, bent up; -1, bent down) 
     void report();         // prints vars to serial 
     
+  protected:
   private:
     int _pin;              // stores the pin of the sensor
     int _threshold;        // stores the threshold value
-    int _calibrate[];    // array to hold readings during calibration
+    int _calibrate[];      // array to hold readings during calibration
     int _calibrateCounter; // counter, determines where in the _calibrate array we store values, loops when full.
     int _restingVal;       // holds the resting (unbent) value of the sensor
     int _currentVal;       // most recent reading from the sensor
     int _previousVal;      // previous reading, for state changes
+    bool _calibrated;	   //if the sensor is not calibrated, will always return neutral
 };
 #endif
